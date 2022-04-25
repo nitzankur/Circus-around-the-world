6a2aa3842b7ae8568680e9fb4b392a03d6f1f19e
Alon Levy <alon153@cs.huji.ac.il>
2022-04-25 13:08:35 +0300

    with reset on end scene

---
+++ Assets/EndSceneManager.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndSceneManager : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadSceneAsync("Openning");
+        }
+    }
+}
